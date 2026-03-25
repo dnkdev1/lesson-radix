2aa69170c1d7eb45d36460b6fb8c0b22dae6c1c5
dnkdev1 <dnkdev@outlook.com>
2026-03-25 09:37:22 +0700

    feat: add setups

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
-
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
 import { App } from './App'
+import "./styles/index.scss"
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
